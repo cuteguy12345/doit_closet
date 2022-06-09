5ffbc514289284969b429fe3580151a09bc8d3b3
DESKTOP-42JJIR1\kyung <kyung8767@naver.com>
2022-06-10 01:04:13 +0900

    best 모듈화

--- tag/tests.py
+++ tag/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Best
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_best_list(self):
+        # 목록 가져오기
+        response = self.client.get('/tag/best/main/')
+
+        # 페이지 로드
+        self.assertEqual(response.status_code, 200)
+
+        # title이 'Best'이다
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Best Coordination')
+
+        #내비 바 체킹
+        navbar = soup.nav
+
+        # 포스트가 없다면 0 있다면 두개면 2
+        self.assertEqual(Best.objects.count(), 0)
+        main_area = soup.find('div', id="main-area")
+        self.assertIn('Not Post', main_area.text)
+
+        post_001 = Best.objects.create(
+            title="첫 번째",
+            content="Hello World!"
+        )
+        post_002 = Best.objects.create(
+            title="두 번째",
+            content="Hello World!!"
+        )
+        self.assertEqual(Best.objects.count(), 2)
+
+        #새로고침했을때
+        response = self.client.get('/tag/best/main/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        #2개 제목이 있다
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        #문구 x
+        self.assertNotIn('Not Post', main_area.text)
